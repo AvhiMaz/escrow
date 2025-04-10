e79b7d2524b4a5eec9c35cc64457c26e5806aaf3
AvhiMaz <avhimazumder5@outlook.com>
2025-04-10 18:04:58 +0530

    anchor: add states
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,16 +1,11 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
+
+mod states;
 
 declare_id!("CoY5d32Ym1WvRsM7ZxcJDMWiixusxNCtWNu1wsJANeGq");
 
 #[program]
 pub mod anchor_escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
