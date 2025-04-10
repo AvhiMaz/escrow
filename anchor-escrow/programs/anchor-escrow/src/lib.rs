462a503956478352a685c729f71aaf0abead90e1
AvhiMaz <avhimazumder5@outlook.com>
2025-04-10 21:05:26 +0530

    anchor: add make instruction
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
+use instructions::*;
 
+mod instructions;
 mod states;
 
 declare_id!("CoY5d32Ym1WvRsM7ZxcJDMWiixusxNCtWNu1wsJANeGq");
@@ -8,4 +10,10 @@ declare_id!("CoY5d32Ym1WvRsM7ZxcJDMWiixusxNCtWNu1wsJANeGq");
 #[program]
 pub mod anchor_escrow {
     use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive_amount: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive_amount, ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
 }
